ef3e49fbe1240afb8f5bca4bb52cd61c2ae2a3be
Arn27 <116111992+Arn27@users.noreply.github.com>
2024-10-20 15:13:35 +0100

    Pridetas front-end

---
+++ resources/js/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="container">
+            <h1 className="text-center">Sveiki atvykę į konferencijų sistemą</h1>
+            {/* Jūsų turinys čia */}
+        </div>
+    );
+}
+
+export default App;
---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title', 'Konferencijų sistema')</title>
+    @viteReactRefresh
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+</head>
+<body>
+    <div id="app">
+        <!-- React aplikacija bus įkelta čia -->
+    </div>
+</body>
+</html>
